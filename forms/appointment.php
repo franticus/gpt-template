7677aef13060b7cb5d8d1d5f96942cbae93cec6c
S.Zelensky <zelenstein@gmail.com>
2023-09-07 17:09:59 +0300

    init

--- forms/appointment.php
+++ forms/appointment.php
@@ -14,7 +14,7 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->subject = 'Online Appointment Form';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -26,9 +26,13 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['name'], 'Name');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['phone'], 'Phone');
+  $contact->add_message( $_POST['date'], 'Appointment Date');
+  $contact->add_message( $_POST['department'], 'Department');
+  $contact->add_message( $_POST['doctor'], 'Doctor');
+  $contact->add_message( $_POST['message'], 'Message');
 
   echo $contact->send();
 ?>
