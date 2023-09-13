1eba741fa0934092ff18e18f2702b51c1de37874
S.Zelensky <zelenstein@gmail.com>
2023-09-13 14:05:28 +0300

    done

--- js/main.js
+++ js/main.js
@@ -39,62 +39,62 @@ function easeInOutCubic(t, b, c, d) {
 }
 
 const textData = {
-  logo: 'TileWorld',
-  heroTitle: `Explore Elegance in Tiles<br />at TileWorld.`,
-  heroDescription: `TileWorld offers a stunning array of ceramic tiles, meticulously curated for discerning customers.`,
-  featuresMainTitle: 'Step into TileWorld',
+  logo: 'RugRealm',
+  heroTitle: `Discover Opulence in Rugs<br />at RugRealm.`,
+  heroDescription: `RugRealm presents an exquisite range of premium rugs, thoughtfully curated for discerning connoisseurs.`,
+  featuresMainTitle: 'Step into RugRealm',
   featuresMainDescription:
-    "Elevate your space with TileWorld's premium ceramic tile collections.",
+    "Elevate your space with RugRealm's luxurious rug collections.",
   featuresList: `<li class="mb-2">
-  <strong>Elegant Tile Selection:</strong> Transform your spaces with our exquisite ceramic tiles and accessories tailored for your style.
+  <strong>Elegant Rug Selection:</strong> Transform your spaces with our exquisite rugs and accessories tailored for your style.
 </li>
 <li class="mb-2">
-  <strong>Luxurious Interiors:</strong> Experience the perfect blend of beauty and functionality with our meticulously crafted tiles.
+  <strong>Luxurious Interiors:</strong> Experience the perfect blend of beauty and functionality with our meticulously crafted rugs.
 </li>
 <li class="mb-2">
-  <strong>Innovative Designs:</strong> Each tile reflects creativity and elegance, ensuring your space is extraordinary.
+  <strong>Innovative Designs:</strong> Each rug showcases creativity and elegance, ensuring your space stands out.
 </li>
 <li class="mb-2">
-  <strong>Personalized Spaces:</strong> Find tiles that cater to your unique preferences, customized just for you.
+  <strong>Personalized Spaces:</strong> Find rugs that cater to your unique preferences, customized just for you.
 </li>`,
-  featuresListCol1T: `Exquisite Tiles<br />for Every Space`,
-  featuresListCol1D: `Discover the essence of luxury interiors with TileWorld's latest collections. Elevate your surroundings with tiles that match your vision.`,
+  featuresListCol1T: `Exquisite Rugs<br />for Every Space`,
+  featuresListCol1D: `Discover the essence of luxury interiors with RugRealm's latest collections. Elevate your surroundings with rugs that match your vision.`,
   featuresListCol2T: `Indulge in Interior<br />Luxury`,
-  featuresListCol2D: `Experience the ultimate beauty and functionality with our sumptuous ceramic tiles. Transform your spaces with premium designs.`,
-  featuresListCol3T: `Distinctive Tile Designs<br />for Every Home`,
-  featuresListCol3D: `Upgrade your living spaces with our distinctive tile designs. Let your home reflect your taste for elegance.`,
-  caseTitle: `Explore Our Tile Catalogue`,
-  caseTitleDesc: `Unveiling TileWorld's Collections`,
-  caseTitleDesc1T: `Elegant Tile Sets`,
-  caseTitleDesc1D: `Indulge in the beauty of our elegant tile sets, designed to transform your spaces with style and sophistication.`,
-  caseTitleDesc2T: `Ceramic Interior Enhancements`,
-  caseTitleDesc2D: `Enhance your home decor with our ceramic interior enhancements, adding a touch of luxury to every room.`,
-  caseTitleDesc3T: `Personalized Tile Solutions`,
-  caseTitleDesc3D: `Customize your spaces with TileWorld, ensuring each tile is tailored to your unique vision and preferences.`,
-  caseTitleDesc4T: `Luxury Flooring Collection`,
-  caseTitleDesc4D: `Experience the luxury of our flooring collection, elevating your home's aesthetics to new heights.`,
-  mob1Title: `Elevate Your Space<br />with TileWorld!`,
-  mob1Desc: `Step into the future of luxury interiors with our opulent ceramic tile collections.`,
-  mob1Desc2: `From personal use to elegant home transformations, TileWorld is redefining the art of ceramic tiles. Dive into a world where every tile is a symbol of beauty.`,
+  featuresListCol2D: `Experience the ultimate beauty and functionality with our sumptuous rugs. Transform your spaces with premium designs.`,
+  featuresListCol3T: `Distinctive Rug Designs<br />for Every Home`,
+  featuresListCol3D: `Upgrade your living spaces with our distinctive rug designs. Let your home reflect your taste for opulence.`,
+  caseTitle: `Explore Our Rug Catalogue`,
+  caseTitleDesc: `Unveiling RugRealm's Collections`,
+  caseTitleDesc1T: `Elegant Rug Sets`,
+  caseTitleDesc1D: `Indulge in the beauty of our elegant rug sets, designed to transform your spaces with style and sophistication.`,
+  caseTitleDesc2T: `Luxury Rug Enhancements`,
+  caseTitleDesc2D: `Enhance your home decor with our luxury rug enhancements, adding a touch of opulence to every room.`,
+  caseTitleDesc3T: `Personalized Rug Solutions`,
+  caseTitleDesc3D: `Customize your spaces with RugRealm, ensuring each rug is tailored to your unique vision and preferences.`,
+  caseTitleDesc4T: `Exquisite Flooring Collection`,
+  caseTitleDesc4D: `Experience the opulence of our flooring collection, elevating your home's aesthetics to new heights.`,
+  mob1Title: `Elevate Your Space<br />with RugRealm!`,
+  mob1Desc: `Step into the future of luxury interiors with our opulent rug collections.`,
+  mob1Desc2: `From personal use to elegant home transformations, RugRealm is redefining the art of rugs. Dive into a world where every rug is a symbol of opulence.`,
   mob2Title: `Where Quality Meets<br />Interior Excellence.`,
-  mob2Desc: `At TileWorld, ceramic tiles are more than just materials. They're about expressing your unique style and enhancing your daily living spaces.`,
-  mob2Desc2: `Embrace the transformative power of innovative tile designs.`,
+  mob2Desc: `At RugRealm, rugs are more than just materials. They're about expressing your unique style and enhancing your daily living spaces.`,
+  mob2Desc2: `Embrace the transformative power of innovative rug designs.`,
   custTitle: `Customer Testimonials`,
-  custDesc: `Hear from our satisfied customers.`,
-  cust1: `TileWorld's ceramic tiles are a game changer. Their beauty is unmatched, and they make my living spaces truly luxurious.`,
-  cust1Name: `Sophie Anderson`,
+  custDesc: `Hear from our satisfied connoisseurs.`,
+  cust1: `RugRealm's rugs are a game changer. Their beauty is unmatched, and they make my living spaces truly opulent.`,
+  cust1Name: `Olivia Bennett`,
   cust1Prof: `Interior Designer`,
-  cust2: `The quality and style of TileWorld tiles are beyond impressive. Every room feels like a work of art.`,
-  cust2Name: `William Turner`,
+  cust2: `The quality and style of RugRealm rugs are beyond impressive. Every room feels like a work of art.`,
+  cust2Name: `Robert Mitchell`,
   cust2Prof: `Homeowner`,
-  cust3: `From everyday spaces to special projects, TileWorld has it all. The epitome of interior elegance.`,
-  cust3Name: `Grace Lewis`,
+  cust3: `From everyday spaces to special projects, RugRealm has it all. The epitome of interior opulence.`,
+  cust3Name: `Sophia Roberts`,
   cust3Prof: `Architect`,
-  cotactTitle: `Ready for Elegant Interiors?<br />Join the TileWorld Experience`,
-  logo2: `TileWorld`,
-  mail: `contact@tileworld.com`,
+  cotactTitle: `Ready for Opulent Interiors?<br />Join the RugRealm Experience`,
+  logo2: `RugRealm`,
+  mail: `contact@rugrealm.com`,
   phone: `+123-456-7890`,
-  logo3: `Copyright © TileWorld. All rights reserved.`,
+  logo3: `Copyright © RugRealm. All rights reserved.`,
 };
 
 const logo = document.querySelector('.text-logo');
