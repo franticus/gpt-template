114c60621a37edc0dcce2dfa890d723feab7092e
S.Zelensky <zelenstein@gmail.com>
2023-09-14 13:24:38 +0300

    done

--- js/main.js
+++ js/main.js
@@ -39,62 +39,62 @@ function easeInOutCubic(t, b, c, d) {
 }
 
 const textData = {
-  logo: 'RideElevation',
-  heroTitle: `Elevate Your Ride<br />with RideElevation Bikes.`,
-  heroDescription: `Immerse yourself in the world of top-notch bicycles, where each ride brings excitement and style.`,
-  featuresMainTitle: 'Embark on a Cycling Adventure with RideElevation',
+  logo: 'EliteTuxShop',
+  heroTitle: `Elevate Your Style<br />with EliteTuxShop Smokings.`,
+  heroDescription: `Immerse yourself in the world of exquisite formalwear, where every occasion is a masterpiece of elegance.`,
+  featuresMainTitle: 'Discover the Elegance of EliteTuxShop',
   featuresMainDescription:
-    "Discover a realm of biking excellence with RideElevation's collection of high-performance bicycles.",
+    "Experience the epitome of sophistication with EliteTuxShop's collection of premium tuxedos and formalwear.",
   featuresList: `<li class="mb-2">
-  <strong>Premium Bicycles:</strong> Elevate your ride with our latest bike models, designed for cycling enthusiasts.
+  <strong>Premium Tuxedos:</strong> Elevate your style with our latest tuxedo designs, tailored for connoisseurs of elegance.
 </li>
 <li class="mb-2">
-  <strong>Unique Designs:</strong> Explore a range of stylish bicycles, meticulously crafted to suit your cycling lifestyle.
+  <strong>Unique Designs:</strong> Explore a range of stylish formalwear, meticulously crafted to suit your special occasions.
 </li>
 <li class="mb-2">
-  <strong>Smooth Riding:</strong> Every bike is engineered for a comfortable journey, ensuring your cycling experiences are exceptional.
+  <strong>Impeccable Fit:</strong> Every tuxedo is tailored for a perfect look, ensuring your events are exceptional.
 </li>
 <li class="mb-2">
-  <strong>Customized Fit:</strong> Personalize your bike to match your unique preferences, tailored for your cycling satisfaction.
+  <strong>Customized Elegance:</strong> Personalize your formalwear to match your unique preferences, tailored for your style satisfaction.
 </li>`,
-  featuresListCol1T: `Top-Tier Bike Collection<br />for Every Cyclist`,
-  featuresListCol1D: `Dive into the world of cycling excellence with RideElevation's latest collection. Enhance your biking journey with bicycles that perfectly resonate with your lifestyle.`,
-  featuresListCol2T: `Exclusive Bike Universe<br />Awaits You`,
-  featuresListCol2D: `Experience the ultimate bike selection with our carefully curated bicycle lineup, adding a touch of sophistication to your every ride.`,
-  featuresListCol3T: `Cycling Paradise<br />for Every Adventure`,
-  featuresListCol3D: `Elevate your cycling journey with our sleek designs and distinctive bike offerings. Let every ride embrace the joy of exploration.`,
-  caseTitle: `Explore Our Bicycle Creations`,
-  caseTitleDesc: `Discover RideElevation's Cycling Revolution`,
-  caseTitleDesc1T: `High-Performance Bicycles`,
-  caseTitleDesc1D: `Indulge in the excellence of our bicycles, meticulously designed to elevate your cycling adventure with precision and style.`,
-  caseTitleDesc2T: `Bike Selections`,
-  caseTitleDesc2D: `Enhance your cycling universe with our carefully crafted bicycles, infusing a touch of sophistication into every pedal stroke.`,
-  caseTitleDesc3T: `Customized Bike Journey`,
-  caseTitleDesc3D: `Customize your bike experience with RideElevation, ensuring each bicycle aligns with your unique cycling vision and preferences.`,
-  caseTitleDesc4T: `Cycling Comfort Enhancements`,
-  caseTitleDesc4D: `Experience the joy of our sleek bicycle designs, elevating your cycling moments to new heights.`,
-  mob1Title: `Elevate Your Cycling<br />Experience with RideElevation!`,
-  mob1Desc: `Step into the world of top-notch bicycles with RideElevation.`,
-  mob1Desc2: `From classic designs to high-performance features, RideElevation is redefining the world of cycling. Immerse yourself in a world where every ride is smooth and stylish.`,
-  mob2Title: `Where Style Meets<br />Performance.`,
-  mob2Desc: `At RideElevation, bicycles are more than just machines. They're about enhancing your cycling journey and creating unforgettable riding moments.`,
-  mob2Desc2: `Embrace the joy of cycling excellence.`,
+  featuresListCol1T: `Elegant Attire Collection<br />for Every Occasion`,
+  featuresListCol1D: `Step into the world of formal elegance with EliteTuxShop's latest collection. Enhance your special events with tuxedos that perfectly resonate with your style.`,
+  featuresListCol2T: `Exclusive Formalwear Selection<br />Awaits You`,
+  featuresListCol2D: `Experience the ultimate formal attire selection with our carefully curated lineup, adding a touch of sophistication to your every event.`,
+  featuresListCol3T: `Style Paradise<br />for Every Celebration`,
+  featuresListCol3D: `Elevate your style journey with our sleek designs and distinctive formalwear offerings. Let every event embrace the joy of elegance.`,
+  caseTitle: `Explore Our Formalwear Creations`,
+  caseTitleDesc: `Discover EliteTuxShop's Elegance Revolution`,
+  caseTitleDesc1T: `High-End Tuxedos`,
+  caseTitleDesc1D: `Indulge in the excellence of our tuxedos, meticulously designed to elevate your special occasions with precision and style.`,
+  caseTitleDesc2T: `Formal Attire Selections`,
+  caseTitleDesc2D: `Enhance your style universe with our carefully crafted formal attire, infusing a touch of sophistication into every event.`,
+  caseTitleDesc3T: `Customized Style Journey`,
+  caseTitleDesc3D: `Customize your formalwear experience with EliteTuxShop, ensuring each outfit aligns with your unique style vision and preferences.`,
+  caseTitleDesc4T: `Elegance Enhancements`,
+  caseTitleDesc4D: `Experience the joy of our sleek formalwear designs, elevating your special moments to new heights.`,
+  mob1Title: `Elevate Your Style<br />Experience with EliteTuxShop!`,
+  mob1Desc: `Step into the world of exquisite formalwear with EliteTuxShop.`,
+  mob1Desc2: `From classic designs to high-end features, EliteTuxShop is redefining the world of elegance. Immerse yourself in a world where every occasion is a masterpiece.`,
+  mob2Title: `Where Elegance Meets<br />Sophistication.`,
+  mob2Desc: `At EliteTuxShop, formal attire is more than just clothing. It's about enhancing your special events and creating unforgettable moments of style.`,
+  mob2Desc2: `Embrace the joy of elegance.`,
   custTitle: `Customer Reviews`,
-  custDesc: `Listen to our satisfied cyclists and biking enthusiasts.`,
-  cust1: `RideElevation's bicycles are a game changer. Their collection is unmatched, and they transformed my cycling experiences into delightful adventures.`,
-  cust1Name: `Sophia Davis`,
-  cust1Prof: `Cycling Enthusiast`,
-  cust2: `The quality and variety of RideElevation's bicycles are beyond impressive. Every ride feels like a cycling masterpiece.`,
-  cust2Name: `John Anderson`,
-  cust2Prof: `Biking Connoisseur`,
-  cust3: `From classic designs to high-performance features, RideElevation has it all. The epitome of cycling excellence.`,
-  cust3Name: `Olivia Roberts`,
-  cust3Prof: `Adventure Cyclist`,
-  contactTitle: `Ready for a Cycling Revolution?<br />Join the RideElevation Journey`,
-  logo2: `RideElevation`,
-  mail: `contact@rideelevation.com`,
+  custDesc: `Listen to our satisfied patrons and style enthusiasts.`,
+  cust1: `EliteTuxShop's formalwear is a game changer. Their collection is unmatched, and they transformed my special occasions into elegant celebrations.`,
+  cust1Name: `Natalie Foster`,
+  cust1Prof: `Elegance Enthusiast`,
+  cust2: `The quality and variety of EliteTuxShop's formal attire are beyond impressive. Every event feels like a stylish masterpiece.`,
+  cust2Name: `William Roberts`,
+  cust2Prof: `Style Connoisseur`,
+  cust3: `From classic designs to high-end features, EliteTuxShop has it all. The epitome of formal elegance.`,
+  cust3Name: `Sophie Anderson`,
+  cust3Prof: `Event Stylist`,
+  contactTitle: `Ready for an Elegance Revolution?<br />Join the EliteTuxShop Journey`,
+  logo2: `EliteTuxShop`,
+  mail: `contact@elitetuxshop.com`,
   phone: `+123-456-7890`,
-  logo3: `Copyright © RideElevation. All rights reserved.`,
+  logo3: `Copyright © EliteTuxShop. All rights reserved.`,
 };
 
 const logo = document.querySelector('.text-logo');
