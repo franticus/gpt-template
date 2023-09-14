af5cd8bd76db3a21492e767bf1285b847b0f3cd0
S.Zelensky <zelenstein@gmail.com>
2023-09-14 13:03:54 +0300

    done

--- js/main.js
+++ js/main.js
@@ -39,62 +39,62 @@ function easeInOutCubic(t, b, c, d) {
 }
 
 const textData = {
-  logo: 'AromaSymphony',
-  heroTitle: `Elevate Your Senses with<br />AromaSymphony's Fragrance Artistry.`,
-  heroDescription: `Immerse yourself in a world of olfactory wonders, where each fragrance is a masterpiece crafted for scent aficionados.`,
-  featuresMainTitle: 'Embark on an Olfactory Journey with AromaSymphony',
+  logo: 'TechWatchMart',
+  heroTitle: `Discover the Future<br />of Timekeeping at TechWatchMart.`,
+  heroDescription: `Immerse yourself in the world of cutting-edge smartwatches, where each timepiece redefines convenience and style.`,
+  featuresMainTitle: 'Embark on a Smartwatch Journey with TechWatchMart',
   featuresMainDescription:
-    "Open the door to a realm of sensory possibilities with AromaSymphony's exquisite fragrances.",
+    "Step into a realm of innovation and possibilities with TechWatchMart's collection of state-of-the-art smartwatches.",
   featuresList: `<li class="mb-2">
-  <strong>Premium Fragrance Collection:</strong> Elevate your sensory experience with our top-tier fragrances, tailored to your olfactory preferences.
+  <strong>Next-Gen Timepieces:</strong> Elevate your wrist game with our latest smartwatches, designed for tech-savvy individuals.
 </li>
 <li class="mb-2">
-  <strong>Exclusive Scents:</strong> Dive into a universe of olfactory excellence with our meticulously crafted fragrance compositions, carefully blended to captivate your senses.
+  <strong>Exclusive Designs:</strong> Explore a universe of stylish smartwatches, meticulously crafted to suit your fashion-forward lifestyle.
 </li>
 <li class="mb-2">
-  <strong>Elegant Presentation:</strong> Every fragrance is a journey into sensory delight, ensuring your experience is exceptional.
+  <strong>Intuitive Features:</strong> Every smartwatch is a gateway to convenience, ensuring your everyday experiences are exceptional.
 </li>
 <li class="mb-2">
-  <strong>Personalized Recommendations:</strong> Customize your fragrance journey to match your individual preferences, tailored for your sensory satisfaction.
+  <strong>Personalized Assistance:</strong> Customize your smartwatch experience to match your unique needs, tailored for your digital satisfaction.
 </li>`,
-  featuresListCol1T: `Top-Notch Fragrance Collection<br />for Every Perfume Lover`,
-  featuresListCol1D: `Explore the pinnacle of olfactory excellence with AromaSymphony's latest collection. Enhance your fragrance adventure with scents that perfectly resonate with your senses.`,
-  featuresListCol2T: `Exclusive Olfactory Universe<br />Awaits You`,
-  featuresListCol2D: `Experience the ultimate olfactory selection with our carefully crafted scents, adding a touch of enchantment to every sensory experience.`,
-  featuresListCol3T: `Sensory Oasis<br />for Every Encounter`,
-  featuresListCol3D: `Enhance your sensory journey with our elegant presentation and distinctive fragrance offerings. Let your senses embrace the magic of aromas.`,
-  caseTitle: `Explore Our Fragrance Creations`,
-  caseTitleDesc: `Discover AromaSymphony's Fragrance Artistry`,
-  caseTitleDesc1T: `Exquisite Fragrance Creations`,
-  caseTitleDesc1D: `Indulge in the excellence of our fragrances, meticulously designed to elevate your sensory adventure with precision and artistry.`,
-  caseTitleDesc2T: `Fragrance Selections`,
-  caseTitleDesc2D: `Enhance your olfactory universe with our carefully curated scents, infusing a touch of enchantment into every sensory encounter.`,
-  caseTitleDesc3T: `Customized Sensory Journey`,
-  caseTitleDesc3D: `Customize your fragrance experience with AromaSymphony, ensuring each scent resonates with your unique sensory vision and preferences.`,
-  caseTitleDesc4T: `Sensory Comfort Enhancements`,
-  caseTitleDesc4D: `Experience the joy of our elegant fragrance presentation, elevating your sensory moments to new heights.`,
-  mob1Title: `Elevate Your Sensory<br />Experience with AromaSymphony!`,
-  mob1Desc: `Step into the world of olfactory bliss with our premium fragrances.`,
-  mob1Desc2: `From classic scents to innovative blends, AromaSymphony is redefining the art of fragrance excellence. Immerse yourself in a world where every aroma heightens your sensory experience.`,
-  mob2Title: `Where Scent Meets<br />Artistry.`,
-  mob2Desc: `At AromaSymphony, fragrance is more than just a scent. It's about enhancing your sensory journey and creating unforgettable moments.`,
-  mob2Desc2: `Embrace the magic of fragrance artistry.`,
+  featuresListCol1T: `Top-Tier Smartwatch Collection<br />for Every Tech Enthusiast`,
+  featuresListCol1D: `Dive into the world of cutting-edge technology with TechWatchMart's latest collection. Enhance your digital journey with timepieces that perfectly resonate with your lifestyle.`,
+  featuresListCol2T: `Exclusive Tech Universe<br />Awaits You`,
+  featuresListCol2D: `Experience the ultimate tech selection with our carefully curated smartwatches, adding a touch of sophistication to your everyday life.`,
+  featuresListCol3T: `Digital Oasis<br />for Every Adventure`,
+  featuresListCol3D: `Elevate your tech journey with our sleek designs and distinctive smartwatch offerings. Let your wrist embrace the magic of innovation.`,
+  caseTitle: `Explore Our Smartwatch Creations`,
+  caseTitleDesc: `Discover TechWatchMart's Digital Revolution`,
+  caseTitleDesc1T: `Cutting-Edge Timepieces`,
+  caseTitleDesc1D: `Indulge in the excellence of our smartwatches, meticulously designed to elevate your digital adventure with precision and style.`,
+  caseTitleDesc2T: `Smartwatch Selections`,
+  caseTitleDesc2D: `Enhance your tech universe with our carefully crafted timepieces, infusing a touch of sophistication into every digital encounter.`,
+  caseTitleDesc3T: `Customized Tech Journey`,
+  caseTitleDesc3D: `Customize your smartwatch experience with TechWatchMart, ensuring each timepiece aligns with your unique tech vision and preferences.`,
+  caseTitleDesc4T: `Digital Comfort Enhancements`,
+  caseTitleDesc4D: `Experience the joy of our sleek smartwatch designs, elevating your digital moments to new heights.`,
+  mob1Title: `Elevate Your Digital<br />Experience with TechWatchMart!`,
+  mob1Desc: `Step into the future of timekeeping with our state-of-the-art smartwatches.`,
+  mob1Desc2: `From classic designs to innovative tech features, TechWatchMart is redefining the world of smartwatches. Immerse yourself in a digital world where every moment is convenient and stylish.`,
+  mob2Title: `Where Technology Meets<br />Fashion.`,
+  mob2Desc: `At TechWatchMart, smartwatches are more than just gadgets. They're about enhancing your digital journey and creating unforgettable tech moments.`,
+  mob2Desc2: `Embrace the magic of tech innovation.`,
   custTitle: `Customer Reviews`,
-  custDesc: `Listen to our satisfied fragrance enthusiasts and scent connoisseurs.`,
-  cust1: `AromaSymphony's fragrances are a game changer. Their collection is unmatched, and they transformed my sensory experiences into delightful adventures.`,
-  cust1Name: `Sophia Johnson`,
-  cust1Prof: `Fragrance Enthusiast`,
-  cust2: `The quality and variety of AromaSymphony's scents are beyond impressive. Every encounter feels like a sensory masterpiece.`,
-  cust2Name: `Lucas Davis`,
-  cust2Prof: `Scent Connoisseur`,
-  cust3: `From classic aromas to rare blends, AromaSymphony has it all. The epitome of fragrance artistry.`,
-  cust3Name: `Ava Williams`,
-  cust3Prof: `Sensory Explorer`,
-  contactTitle: `Ready for an Exquisite Sensory Experience?<br />Join the AromaSymphony Journey`,
-  logo2: `AromaSymphony`,
-  mail: `contact@aromasymphony.com`,
+  custDesc: `Listen to our satisfied tech enthusiasts and digital connoisseurs.`,
+  cust1: `TechWatchMart's smartwatches are a game changer. Their collection is unmatched, and they transformed my digital experiences into delightful adventures.`,
+  cust1Name: `Emily Miller`,
+  cust1Prof: `Tech Enthusiast`,
+  cust2: `The quality and variety of TechWatchMart's smartwatches are beyond impressive. Every interaction feels like a digital masterpiece.`,
+  cust2Name: `Daniel White`,
+  cust2Prof: `Digital Connoisseur`,
+  cust3: `From classic designs to tech innovations, TechWatchMart has it all. The epitome of digital excellence.`,
+  cust3Name: `Michaela Robinson`,
+  cust3Prof: `Tech Explorer`,
+  contactTitle: `Ready for a Tech Revolution?<br />Join the TechWatchMart Journey`,
+  logo2: `TechWatchMart`,
+  mail: `contact@techwatchmart.com`,
   phone: `+123-456-7890`,
-  logo3: `Copyright © AromaSymphony. All rights reserved.`,
+  logo3: `Copyright © TechWatchMart. All rights reserved.`,
 };
 
 const logo = document.querySelector('.text-logo');
