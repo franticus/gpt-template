28931157b2ba11274d871e9009f3c237f7af0838
S.Zelensky <zelenstein@gmail.com>
2023-09-19 12:04:03 +0300

    done

--- js/main.js
+++ js/main.js
@@ -39,59 +39,59 @@ function easeInOutCubic(t, b, c, d) {
 }
 
 const textData = {
-  logo: `SwimGear`,
-  heroTitle: `Dive into Crystal-Clear Waters with SwimGear's Swimwear`,
-  heroDescription: `SwimGear offers a diverse range of swimwear and accessories to elevate your aquatic adventures.`,
-  featuresMainTitle: `Experience the SwimGear Difference`,
-  featuresMainDescription: `Our team of water enthusiasts is dedicated to enhancing your swimming experience with top-quality gear choices.`,
+  logo: `QuantumSight`,
+  heroTitle: `Experience Visual Brilliance with QuantumSight TVs`,
+  heroDescription: `QuantumSight offers a diverse range of cutting-edge televisions to transform your viewing experience.`,
+  featuresMainTitle: `Discover the QuantumSight Advantage`,
+  featuresMainDescription: `Our team of TV enthusiasts is dedicated to enhancing your entertainment with top-quality television choices.`,
   featuresList: `<li class="mb-2">
-    <strong>Stylish Swimwear Collection:</strong> Explore our wide range of swimwear to elevate your look and enhance your water adventures.
+    <strong>Innovative TV Technology:</strong> Explore our advanced television technology to elevate your visual experience.
   </li>
   <li class="mb-2">
-    <strong>Aquatic Tips & Insights:</strong> Let us provide you with expert advice and tips to keep your swimming experience safe and stylish with our swim gear.
+    <strong>Entertainment Tips & Insights:</strong> Let us provide you with expert advice and tips to enrich your viewing experience with our TVs.
   </li>
   <li class="mb-2">
-    <strong>Swimming Accessories:</strong> Find the perfect additions to enhance your swimming performance and complete your aquatic ensemble.
+    <strong>Home Theater Accessories:</strong> Find the perfect additions to enhance your home theater setup and complete your entertainment ensemble.
   </li>
   <li class="mb-2">
-    <strong>Expert Water Support:</strong> Learn how to enjoy clear waters and aquatic fun in any situation with personalized advice from our experts.
+    <strong>Expert Entertainment Support:</strong> Learn how to enjoy immersive entertainment in any situation with personalized advice from our experts.
   </li>`,
-  featuresListCol1T: `Fashionable Swimwear Collection`,
-  featuresListCol1D: `SwimGear guarantees a wide variety of swimwear to suit your style and enhance your aquatic adventures.`,
-  featuresListCol2T: `Experienced Water Enthusiasts`,
-  featuresListCol2D: `Our skilled water enthusiasts are passionate about helping you find the perfect swim gear to elevate your aquatic look.`,
-  featuresListCol3T: `Personalized Water Support`,
-  featuresListCol3D: `SwimGear caters to your specific aquatic needs, offering personalized solutions and expert guidance.`,
-  caseTitle: `Our Swim Gear Collections`,
-  caseTitleDesc: `Explore SwimGear's Aquatic Selection`,
-  caseTitleDesc1T: `Contemporary Swimwear`,
-  caseTitleDesc1D: `Discover our range of contemporary swimwear to elevate your style and enhance your aquatic adventures with flair.`,
-  caseTitleDesc2T: `High-Performance Water Gear`,
-  caseTitleDesc2D: `Experience high-performance water gear designed to keep your swimming experience clear and fashionable.`,
-  caseTitleDesc3T: `Adventure-Ready Swim Gear`,
-  caseTitleDesc3D: `Find adventure-ready swim gear that provides style and safety while enjoying the best aquatic experiences.`,
-  mob1Title: `Dive into the World of Aquatic Fun<br />with SwimGear!`,
-  mob1Desc: `SwimGear offers a world of water solutions where you can swim confidently and look stylish.`,
-  mob1Desc2: `Join us and let our swim gear enhance your aquatic adventures and elevate your style.`,
-  mob2Title: `Where Style Meets Water Fun<br />Your Aquatic Solutions, Your Way.`,
-  mob2Desc: `At SwimGear, we believe that everyone deserves clear waters and fashionable swimwear. Let us help you find the perfect gear.`,
-  mob2Desc2: `Elevate your aquatic look and enjoy safe and stylish water experiences with our top-quality gear.`,
-  custTitle: `Satisfied Customers, Stylish Aquatic Adventures`,
-  custDesc: `Hear what our happy customers have to say about their SwimGear experiences.`,
-  cust1: `I'm always amazed by the style and functionality of SwimGear's swimwear. They've elevated my aquatic adventures.`,
-  cust1Name: `Sophia Turner`,
-  cust1Prof: `Water Enthusiast`,
-  cust2: `SwimGear made finding the perfect swim gear effortless. It's a game-changer for any water activity.`,
-  cust2Name: `Daniel Robinson`,
-  cust2Prof: `Beach Explorer`,
-  cust3: `I love recommending SwimGear accessories to friends. They add the perfect finishing touch to any aquatic look.`,
-  cust3Name: `Isabella Martinez`,
-  cust3Prof: `Style Maven`,
-  contactTitle: `Ready to Dive into Stylish Aquatic Adventures? Contact SwimGear Today`,
-  logo2: `SwimGear`,
-  mail: `info@swimgear.com`,
+  featuresListCol1T: `Cutting-Edge TV Technology`,
+  featuresListCol1D: `QuantumSight guarantees the latest TV technology to transform your viewing experience.`,
+  featuresListCol2T: `TV Enthusiasts at Your Service`,
+  featuresListCol2D: `Our passionate TV enthusiasts are dedicated to helping you find the perfect television to elevate your entertainment.`,
+  featuresListCol3T: `Personalized Entertainment Support`,
+  featuresListCol3D: `QuantumSight caters to your specific entertainment needs, offering personalized solutions and expert guidance.`,
+  caseTitle: `Our TV Collections`,
+  caseTitleDesc: `Explore QuantumSight's TV Selection`,
+  caseTitleDesc1T: `Smart TV Selection`,
+  caseTitleDesc1D: `Discover our range of smart TVs to enhance your viewing experience with convenience and innovation.`,
+  caseTitleDesc2T: `High-Definition Visuals`,
+  caseTitleDesc2D: `Experience high-definition visuals designed to bring your favorite shows and movies to life.`,
+  caseTitleDesc3T: `Cinema-Quality Home Theater`,
+  caseTitleDesc3D: `Create a cinema-quality home theater with QuantumSight's top-quality TVs for the ultimate entertainment experience.`,
+  mob1Title: `Immerse Yourself in Entertainment<br />with QuantumSight!`,
+  mob1Desc: `QuantumSight offers a world of entertainment solutions where you can enjoy stunning visuals and immersive sound.`,
+  mob1Desc2: `Join us and let our TVs transform your entertainment experience.`,
+  mob2Title: `Where Quality Meets Entertainment Fun<br />Your Entertainment Solutions, Your Way.`,
+  mob2Desc: `At QuantumSight, we believe that everyone deserves cutting-edge entertainment. Let us help you find the perfect TV.`,
+  mob2Desc2: `Elevate your viewing experience and enjoy immersive entertainment with our top-quality TVs.`,
+  custTitle: `Satisfied Customers, Immersive Entertainment Experiences`,
+  custDesc: `Hear what our happy customers have to say about their QuantumSight TV experiences.`,
+  cust1: `I'm always amazed by the quality and features of QuantumSight's TVs. They've transformed my entertainment experience.`,
+  cust1Name: `Emily Mitchell`,
+  cust1Prof: `Home Entertainment Enthusiast`,
+  cust2: `QuantumSight made finding the perfect TV effortless. It's a game-changer for any movie night.`,
+  cust2Name: `David Reynolds`,
+  cust2Prof: `Film Buff`,
+  cust3: `I love recommending QuantumSight accessories to friends. They add the perfect finishing touch to any home theater setup.`,
+  cust3Name: `Jennifer Parker`,
+  cust3Prof: `Tech Enthusiast`,
+  contactTitle: `Ready to Elevate Your Entertainment? Contact QuantumSight Today`,
+  logo2: `QuantumSight`,
+  mail: `info@quantumsighttv.com`,
   phone: `+123-456-7890`,
-  logo3: `Copyright © SwimGear. All rights reserved.`,
+  logo3: `Copyright © QuantumSight. All rights reserved.`,
 };
 
 const logo = document.querySelector('.text-logo');
