9b248ab8f6e4f12752858c7ec172c8ec762ed4a6
S.Zelensky <zelenstein@gmail.com>
2023-09-19 11:44:13 +0300

    done

--- js/main.js
+++ js/main.js
@@ -39,59 +39,59 @@ function easeInOutCubic(t, b, c, d) {
 }
 
 const textData = {
-  logo: `SolarGaze`,
-  heroTitle: `Discover a Brighter World with SolarGaze Sunglasses`,
-  heroDescription: `SolarGaze offers a stunning selection of sunglasses to elevate your style and protect your eyes.`,
-  featuresMainTitle: `Experience the SolarGaze Difference`,
-  featuresMainDescription: `Our team of eyewear experts is dedicated to enhancing your vision with top-notch sunglass choices.`,
+  logo: `LensCrafters`,
+  heroTitle: `Discover Clear Vision with LensCrafters Eyewear`,
+  heroDescription: `LensCrafters offers a diverse range of eyeglasses to elevate your sight and style.`,
+  featuresMainTitle: `Experience the LensCrafters Difference`,
+  featuresMainDescription: `Our team of eyewear enthusiasts is dedicated to enhancing your vision with top-quality eyeglass choices.`,
   featuresList: `<li class="mb-2">
-    <strong>Stylish Sunglass Collection:</strong> Explore our wide range of sunglasses to elevate your look and shield your eyes from the sun.
+    <strong>Stylish Eyeglass Collection:</strong> Explore our wide range of eyeglasses to elevate your look and enhance your vision.
   </li>
   <li class="mb-2">
-    <strong>Eye Protection Insights:</strong> Let us provide you with expert advice and tips to keep your eyes safe and stylish with our sunglasses.
+    <strong>Eye Health Insights:</strong> Let us provide you with expert advice and tips to keep your eyes healthy and stylish with our eyeglasses.
   </li>
   <li class="mb-2">
-    <strong>Sunglass Accessories:</strong> Find the perfect additions to enhance your sunglass performance and complete your fashionable ensemble.
+    <strong>Eyeglass Accessories:</strong> Find the perfect additions to enhance your eyeglass performance and complete your fashionable ensemble.
   </li>
   <li class="mb-2">
-    <strong>Expert Vision Support:</strong> Learn how to enjoy clear vision and sun protection in any situation with personalized advice from our experts.
+    <strong>Expert Vision Support:</strong> Learn how to enjoy clear vision and eye protection in any situation with personalized advice from our experts.
   </li>`,
-  featuresListCol1T: `Fashionable Sunglass Collection`,
-  featuresListCol1D: `SolarGaze guarantees a wide variety of sunglasses to suit your style and protect your eyes from the sun's rays.`,
+  featuresListCol1T: `Fashionable Eyeglass Collection`,
+  featuresListCol1D: `LensCrafters guarantees a wide variety of eyeglasses to suit your style and enhance your vision.`,
   featuresListCol2T: `Experienced Eyewear Enthusiasts`,
-  featuresListCol2D: `Our skilled eyewear enthusiasts are passionate about helping you find the perfect sunglasses to elevate your look.`,
+  featuresListCol2D: `Our skilled eyewear enthusiasts are passionate about helping you find the perfect eyeglasses to elevate your look.`,
   featuresListCol3T: `Personalized Vision Support`,
-  featuresListCol3D: `SolarGaze caters to your specific eyewear needs, offering personalized solutions and expert guidance.`,
-  caseTitle: `Our Sunglass Collections`,
-  caseTitleDesc: `Explore SolarGaze's Sunglass Selection`,
-  caseTitleDesc1T: `Modern Fashion Sunglasses`,
-  caseTitleDesc1D: `Discover our range of modern fashion sunglasses to elevate your style and protect your eyes with flair.`,
-  caseTitleDesc2T: `High-Performance Eye Protection`,
-  caseTitleDesc2D: `Experience high-performance eye protection solutions designed to keep your vision clear and fashionable.`,
-  caseTitleDesc3T: `Sun-Savvy Sunglasses`,
-  caseTitleDesc3D: `Find sun-savvy sunglasses that provide style and eye safety while enjoying the great outdoors.`,
-  mob1Title: `See the World Brighter<br />with SolarGaze!`,
-  mob1Desc: `SolarGaze offers a world of eyewear solutions where you can see clearly and look stylish.`,
-  mob1Desc2: `Join us and let our sunglasses enhance your vision and elevate your style.`,
-  mob2Title: `Where Style Meets Eye Safety<br />Your Sunglass Solutions, Your Way.`,
-  mob2Desc: `At SolarGaze, we believe that everyone deserves clear vision and fashionable eyewear. Let us help you find the perfect sunglasses.`,
-  mob2Desc2: `Transform your look and protect your eyes with our top-quality sunglass solutions.`,
+  featuresListCol3D: `LensCrafters caters to your specific eyewear needs, offering personalized solutions and expert guidance.`,
+  caseTitle: `Our Eyeglass Collections`,
+  caseTitleDesc: `Explore LensCrafters' Eyeglass Selection`,
+  caseTitleDesc1T: `Contemporary Fashion Eyeglasses`,
+  caseTitleDesc1D: `Discover our range of contemporary fashion eyeglasses to elevate your style and enhance your vision with flair.`,
+  caseTitleDesc2T: `High-Performance Eye Enhancement`,
+  caseTitleDesc2D: `Experience high-performance eye enhancement solutions designed to keep your vision clear and fashionable.`,
+  caseTitleDesc3T: `Vision-Boosting Eyeglasses`,
+  caseTitleDesc3D: `Find vision-boosting eyeglasses that provide style and eye health while enjoying the best vision possible.`,
+  mob1Title: `See the World Brighter<br />with LensCrafters!`,
+  mob1Desc: `LensCrafters offers a world of eyewear solutions where you can see clearly and look stylish.`,
+  mob1Desc2: `Join us and let our eyeglasses enhance your vision and elevate your style.`,
+  mob2Title: `Where Style Meets Eye Health<br />Your Eyeglass Solutions, Your Way.`,
+  mob2Desc: `At LensCrafters, we believe that everyone deserves clear vision and fashionable eyewear. Let us help you find the perfect eyeglasses.`,
+  mob2Desc2: `Transform your look and protect your eyes with our top-quality eyeglass solutions.`,
   custTitle: `Satisfied Customers, Stylish Vision`,
-  custDesc: `Hear what our happy customers have to say about their SolarGaze sunglass experiences.`,
-  cust1: `I'm always amazed by the style and clarity SolarGaze sunglasses provide. They've elevated my fashion game.`,
-  cust1Name: `Sophia Roberts`,
-  cust1Prof: `Fashion Blogger`,
-  cust2: `SolarGaze made finding the perfect sunglasses effortless. It's a game-changer for any sunny day.`,
-  cust2Name: `James Turner`,
-  cust2Prof: `Outdoor Enthusiast`,
-  cust3: `I love recommending SolarGaze sunglass accessories to friends. They add the perfect finishing touch to any look.`,
-  cust3Name: `Emily Walker`,
-  cust3Prof: `Style Influencer`,
-  contactTitle: `Ready to Enhance Your Vision? Contact SolarGaze Today`,
-  logo2: `SolarGaze`,
-  mail: `info@solarGaze.com`,
+  custDesc: `Hear what our happy customers have to say about their LensCrafters eyeglass experiences.`,
+  cust1: `I'm always amazed by the style and clarity LensCrafters eyeglasses provide. They've elevated my fashion game.`,
+  cust1Name: `Olivia Mitchell`,
+  cust1Prof: `Fashion Enthusiast`,
+  cust2: `LensCrafters made finding the perfect eyeglasses effortless. It's a game-changer for any day.`,
+  cust2Name: `Michael Anderson`,
+  cust2Prof: `Daily Commuter`,
+  cust3: `I love recommending LensCrafters eyeglass accessories to friends. They add the perfect finishing touch to any look.`,
+  cust3Name: `Emma Davis`,
+  cust3Prof: `Style Maven`,
+  contactTitle: `Ready to Enhance Your Vision? Contact LensCrafters Today`,
+  logo2: `LensCrafters`,
+  mail: `info@lenscrafters.com`,
   phone: `+123-456-7890`,
-  logo3: `Copyright © SolarGaze. All rights reserved.`,
+  logo3: `Copyright © LensCrafters. All rights reserved.`,
 };
 
 const logo = document.querySelector('.text-logo');
