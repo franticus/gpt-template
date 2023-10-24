f38e20c1f67b4f165143a495959186445d4ae8b7
S.Zelensky <zelenstein@gmail.com>
2023-10-24 19:49:15 +0300

    privacy

--- thanks_page.php
+++ thanks_page.php
@@ -1,31 +1,8 @@
-<?php
-
-$data = implode("\n", $_POST);
-
-$domain = $_SERVER['HTTP_HOST'];
-$to = "lead@".$domain; 
-$subject = "Lead";
-$message = $data;
-$headers = "From: sender@".$domain;
-
-if(mail($to, $subject, $message, $headers)) {
-    //echo "Письмо успешно отправлено!";
-}
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="ru">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Piazzolla:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
-    />
     <title>Thank you!</title>
 
     <style>
@@ -245,4 +222,3 @@ if(mail($to, $subject, $message, $headers)) {
     </script>
   </body>
 </html>
-Ï
