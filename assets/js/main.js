46373829ddaa873366d448442d3a9244a76c66c9
S.Zelensky <zelenstein@gmail.com>
2023-11-08 16:27:06 +0300

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+/**
+* Template Name: Flattern
+* Updated: Sep 18 2023 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/flattern-multipurpose-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
 (function() {
   "use strict";
 
@@ -149,42 +156,6 @@
   });
 
   /**
-   * Clients Slider
-   */
-  new Swiper('.clients-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 40
-      },
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 60
-      },
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 80
-      },
-      992: {
-        slidesPerView: 6,
-        spaceBetween: 120
-      }
-    }
-  });
-
-  /**
    * Skills animation
    */
   let skilsContent = select('.skills-content');
@@ -224,6 +195,9 @@
         portfolioIsotope.arrange({
           filter: this.getAttribute('data-filter')
         });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
       }, true);
     }
 
@@ -254,8 +228,15 @@
   });
 
   /**
-   * Initiate Pure Counter 
+   * Animation on scroll
    */
-  new PureCounter();
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
 
 })()
