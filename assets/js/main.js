0e921a66f986a32cdf4dc373c25fc19692445def
S.Zelensky <zelenstein@gmail.com>
2023-11-22 15:55:42 +0300

    priv done

--- assets/js/main.js
+++ assets/js/main.js
@@ -168,4 +168,30 @@
     })
   });
 
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      },
+    });
+  });
+
 })()
