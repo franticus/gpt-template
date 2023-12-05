413a96504ce06592c73c49bc17a9fd1d87414708
S.Zelensky <zelenstein@gmail.com>
2023-12-05 12:35:28 +0300

    init

--- assets/js/mscript.js
+++ assets/js/mscript.js
@@ -126,3 +126,17 @@ var hostname = window.location.hostname;
 domainElement.forEach((elem) => {
   elem.innerHTML = `<a href="mailto:info@${hostname}" target="_blank">info@${hostname}</a>`;
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const faqItems = document.querySelectorAll(".faq-item");
+
+  faqItems.forEach((item) => {
+      item.addEventListener("click", () => {
+          faqItems.forEach((otherItem) => {
+              if (otherItem !== item) {
+                  otherItem.querySelector(".faq-toggle").checked = false;
+              }
+          });
+      });
+  });
+});
