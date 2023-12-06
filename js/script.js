182aa7f6ceaa57bac59946c23ea4cb55550c28c5
S.Zelensky <zelenstein@gmail.com>
2023-12-06 21:11:58 +0300

    done

--- js/script.js
+++ js/script.js
@@ -12,19 +12,27 @@ function replaceTextContent(node) {
 
 replaceTextContent(document.body);
 
-if (
-  localStorage.getItem('cookieConsent') !== 'accepted' &&
-  localStorage.getItem('cookieConsent') !== 'rejected'
-) {
-  document.getElementById('cookieBanner').style.display = 'block';
+const cookieBanner = document.getElementById('cookieBanner') || 0;
+
+if (!!cookieBanner) {
+  if (
+    localStorage.getItem('cookieConsent') !== 'accepted' &&
+    localStorage.getItem('cookieConsent') !== 'rejected'
+  ) {
+    document.getElementById('cookieBanner').style.display = 'block';
+  }
+
+  document
+    .getElementById('acceptCookies')
+    .addEventListener('click', function () {
+      localStorage.setItem('cookieConsent', 'accepted');
+      document.getElementById('cookieBanner').style.display = 'none';
+    });
+
+  document
+    .getElementById('rejectCookies')
+    .addEventListener('click', function () {
+      localStorage.setItem('cookieConsent', 'rejected');
+      document.getElementById('cookieBanner').style.display = 'none';
+    });
 }
-
-document.getElementById('acceptCookies').addEventListener('click', function () {
-  localStorage.setItem('cookieConsent', 'accepted');
-  document.getElementById('cookieBanner').style.display = 'none';
-});
-
-document.getElementById('rejectCookies').addEventListener('click', function () {
-  localStorage.setItem('cookieConsent', 'rejected');
-  document.getElementById('cookieBanner').style.display = 'none';
-});
