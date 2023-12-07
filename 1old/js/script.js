9ec1741bb8b294c526c9cd64fe6b40ac5173a8eb
S.Zelensky <zelenstein@gmail.com>
2023-12-07 18:55:40 +0300

    policy

--- 1old/js/script.js
+++ 1old/js/script.js
@@ -1,17 +1,3 @@
-var currentDomain = window.location.hostname;
-
-function replaceTextContent(node) {
-  if (node.nodeType === Node.TEXT_NODE) {
-    node.nodeValue = node.nodeValue.replace(/ambientia\.com/g, currentDomain);
-  } else if (node.nodeType === Node.ELEMENT_NODE) {
-    for (var i = 0; i < node.childNodes.length; i++) {
-      replaceTextContent(node.childNodes[i]);
-    }
-  }
-}
-
-replaceTextContent(document.body);
-
 const cookieBanner = document.getElementById('cookieBanner') || 0;
 
 if (!!cookieBanner) {
