de8ec48ca28ee883463f113489f8be58b8918d18
S.Zelensky <zelenstein@gmail.com>
2023-12-07 13:40:38 +0300

    mail

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
 var currentDomain = window.location.hostname;
+var spans = document.querySelectorAll('#replace');
 
-function replaceTextContent(node) {
-  if (node.nodeType === Node.TEXT_NODE) {
-    node.nodeValue = node.nodeValue.replace(/ambientia\.com/g, currentDomain);
-  } else if (node.nodeType === Node.ELEMENT_NODE) {
-    for (var i = 0; i < node.childNodes.length; i++) {
-      replaceTextContent(node.childNodes[i]);
-    }
-  }
-}
+spans.forEach(function(span) {
+  span.innerHTML = `<a href="mailto:info@${currentDomain}"> info@${currentDomain} </a>`;
+});
 
-replaceTextContent(document.body);
 
 const cookieBanner = document.getElementById('cookieBanner') || 0;
 
