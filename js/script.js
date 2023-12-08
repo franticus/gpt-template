dc1ed28c0a3337c1ed330e93113fc794d244254f
S.Zelensky <zelenstein@gmail.com>
2023-12-08 12:16:33 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const mobileMenuOverlay = document.querySelector('.mobile-menu-overlay');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  burgerMenu.addEventListener('click', function () {
+      mobileMenu.classList.toggle('active');
+      mobileMenuOverlay.classList.toggle('active');
+  });
+
+  mobileMenuOverlay.addEventListener('click', function () {
+      mobileMenu.classList.remove('active');
+      mobileMenuOverlay.classList.remove('active');
+  });
+
+  const mobileMenuLinks = mobileMenu.querySelectorAll('a');
+
+    mobileMenuLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            mobileMenu.classList.remove('active');
+            mobileMenuOverlay.classList.remove('active');
+        });
+    });
+});
