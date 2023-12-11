b18f8ebeac292276cb41099d78c287c3333831e2
S.Zelensky <zelenstein@gmail.com>
2023-12-11 11:15:05 +0300

    cookie

--- js/script.js
+++ js/script.js
@@ -22,3 +22,25 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const cookieConsent = document.getElementById('cookieConsent');
+  const acceptButton = document.getElementById('acceptButton');
+  const rejectButton = document.getElementById('rejectButton');
+
+  const hasAcceptedCookies = localStorage.getItem('cookieConsent') === 'true';
+
+  if (!hasAcceptedCookies) {
+    cookieConsent.style.display = 'block';
+  }
+
+  acceptButton.addEventListener('click', function () {
+    localStorage.setItem('cookieConsent', 'true');
+    cookieConsent.style.display = 'none';
+  });
+
+  rejectButton.addEventListener('click', function () {
+    localStorage.setItem('cookieConsent', 'false');
+    cookieConsent.style.display = 'none';
+  });
+});
