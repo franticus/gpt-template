e10a3b077c65a0561f9f90842025f7b643c0ef89
S.Zelensky <zelenstein@gmail.com>
2023-12-12 13:23:05 +0300

    done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const burgerMenu = document.querySelector('.burger-menu');
-  const navList = document.querySelector('.nav-list');
-
-  burgerMenu.addEventListener('click', function () {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navList = document.querySelector('.nav-list');
+    const overlay = document.querySelector('.overlay');
+  
+    burgerMenu.addEventListener('click', function () {
       navList.classList.toggle('active');
-  });
-
-  document.addEventListener('click', function (event) {
+      overlay.classList.toggle('active');
+      document.body.classList.toggle('no-scroll');
+    });
+  
+    document.addEventListener('click', function (event) {
       if (!navList.contains(event.target) && event.target !== burgerMenu) {
-          navList.classList.remove('active');
+        navList.classList.remove('active');
+        overlay.classList.remove('active');
+        document.body.classList.remove('no-scroll');
       }
+    });
   });
-});
