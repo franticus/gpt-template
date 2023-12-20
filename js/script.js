455951d2a65e59bb9ea21e3b6a56f6f356365f52
S.Zelensky <zelenstein@gmail.com>
2023-12-20 18:10:17 +0300

    home

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const burgerMenu = document.querySelector('.burger-menu');
-    const navList = document.querySelector('.nav-list');
-    const overlay = document.querySelector('.overlay');
-  
-    burgerMenu.addEventListener('click', function () {
-      navList.classList.toggle('active');
-      overlay.classList.toggle('active');
-      document.body.classList.toggle('no-scroll');
-    });
-  
-    document.addEventListener('click', function (event) {
-      if (!navList.contains(event.target) && event.target !== burgerMenu) {
-        navList.classList.remove('active');
-        overlay.classList.remove('active');
-        document.body.classList.remove('no-scroll');
-      }
-    });
+  const overlay = document.querySelector('.overlay');
+  const navList = document.querySelector('.nav-list');
+  const burgerMenu = document.querySelector('.burger-menu');
+
+  burgerMenu.addEventListener('click', function () {
+    overlay.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
+    navList.classList.toggle('active');
   });
+
+  document.addEventListener('click', function (event) {
+    if (!navList.contains(event.target) && event.target !== burgerMenu) {
+      overlay.classList.remove('active');
+      document.body.classList.remove('no-scroll');
+      navList.classList.remove('active');
+    }
+  });
+});
