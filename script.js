68651ece704aeec7d1af147c0efd989759de10cd
S.Zelensky <zelenstein@gmail.com>
2024-02-01 19:27:46 +0300

    text

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const menuButton = document.querySelector('.sdkuhkbd');
-  const menu = document.querySelector('.nncbcvx');
+  const menuButton = document.querySelector('.deLaQyKVGi');
+  const menu = document.querySelector('.IiVNPsouRD');
   menuButton.addEventListener('click', function () {
     menu.classList.toggle('show');
 
     if (menu.classList.contains('show')) {
       document.body.style.overflow = 'hidden';
-      overlay.style.display = 'block';
     } else {
       document.body.style.overflow = 'auto';
-      overlay.style.display = 'none';
     }
   });
 
   document.addEventListener('click', function (event) {
     if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
       menu.classList.remove('show');
-      overlay.style.display = 'none';
       document.body.style.overflow = 'auto';
     }
   });
