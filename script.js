4f915ac7afc7ad07c5699b32188ee0fedc6346a5
S.Zelensky <zelenstein@gmail.com>
2024-02-14 15:28:19 +0300

    done

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const body = document.querySelector('body');
   const burger = document.querySelector('.QxwGxutbRo');
   const navLinks = document.querySelector('.RTRcmzcmHk');
 
   burger.addEventListener('click', function () {
+    if (navLinks.classList.contains('active')) {
+      body.style.overflow = 'visible';
+    } else {
+      body.style.overflow = 'hidden';
+    }
     navLinks.classList.toggle('active');
     burger.classList.toggle('toggle');
   });
 
   navLinks.addEventListener('click', function () {
     navLinks.classList.remove('active');
+    body.style.overflow = 'visible';
     burger.classList.remove('toggle');
   });
 });
